5beaa2447b33be143db605d9e57fd08922bdf7f9
Nithin <nithin@learneon.com>
2024-10-08 15:00:00 +0530

    savec ahcnages same as before

--- pod/eonpod/pod/classes/test pauseunpause.py
+++ pod/eonpod/pod/classes/test pauseunpause.py
@@ -1,10 +1,10 @@
 import subprocess
 import os
 from datetime import datetime
-from django.conf import settings
 import logging
 import re
 import pytz
+import time
 
 # Get the logger instance for the 'pod' app
 logger = logging.getLogger('pod')
@@ -16,18 +16,18 @@ class Recorder:
         self.grab_process = None
         self.stream_port = 8090  # You can choose a different port if needed
         self.timestamp = None
-        self.timezone = pytz.timezone('Asia/Kolkata') 
+        self.timezone = pytz.timezone('Asia/Kolkata')
         self.subject = None
         self.part = 0
-        self.media_folderpath = os.path.join(settings.BASE_DIR, 'media', 'processed_files')
+        base_dir = "/home/eon/Desktop/rock5/pod/eonpod/media/processed_files"  # Change this to your desired folder path
+        self.media_folderpath = os.path.join(base_dir)
+        print(f"Media folder path: {self.media_folderpath}")
         self.camera_url = 'rtsp://admin:hik@9753@192.168.0.252:554/Streaming/Channels/101'
         self.devnull = open(os.devnull, 'w')
         logger.info("Initialized Recorder")
 
-
     def update_timestamp(self):
         self.timestamp = datetime.now(self.timezone).strftime("%d-%m-%Y_%H-%M-%S")
-
 
     def get_audio_device_info(self):
         # Run the arecord -l command and capture the output
@@ -36,7 +36,7 @@ class Recorder:
         # Check if the command was successful
         if result.returncode != 0:
             logger.error("Error running arecord command.")
-            return None, None
+            return None
 
         # Decode the output
         output = result.stdout
@@ -45,62 +45,79 @@ class Recorder:
         device_pattern = re.compile(r'card (\d+): .*?\[.*?USB Composite Device.*?\], device (\d+):')
 
         # Search for the device in the output
-        matches = device_pattern.findall(output)
+        match = device_pattern.search(output)
 
-        # Return the first found card and device number, if any
-        if matches:
-            card, device = matches[0]
-            logger.info(f"Found audio device: Card {card}, Device {device}")
-            return f"hw:{card},{device}"
+        # Return the device path (e.g., hw:0,0) if a match is found
+        if match:
+            card, device = match.groups()
+            device_str = f"hw:{card},{device}"
+            logger.info(f"Found audio device: {device_str}")
+            return device_str
         else:
             logger.warning("No USB Composite Device found.")
-            return None, None
+            return None
+
 
     def pause_recording(self):
         self.stop_recording()
 
+
     def unpause_recording(self):
         self.part += 1
         self.start_recording(self.subject, self.part)
-    
+
+
     def concat_recording_parts(self):
         """Concatenate all recorded parts into one final MP4 file."""
         try:
             # Directory where parts are saved
             files_dir = os.path.join(self.media_folderpath, self.subject, self.timestamp)
-            
-            # Find all part files in the directory
+
+            # Find all part files in the directory that match the recorded video naming pattern
             part_files = sorted(
-                [f for f in os.listdir(files_dir) if f.endswith('.mp4')],
-                key=lambda x: int(re.search(r'part_(\d+)', x).group(1))  # Sort by part number
+                [f for f in os.listdir(files_dir) 
+                if re.match(r'^\d{2}-\d{2}-\d{4}_\d{2}-\d{2}-\d{2}_recorded_video_\d+\.mp4$', f)],  # Match the full naming pattern
+                key=lambda x: int(re.search(r'_(\d+)\.mp4', x).group(1))  # Sort by part number
             )
-            
+
             # Create a file with the list of files to be concatenated
             concat_list_path = os.path.join(files_dir, "concat_list.txt")
             with open(concat_list_path, 'w') as concat_list:
                 for part_file in part_files:
                     concat_list.write(f"file '{os.path.join(files_dir, part_file)}'\n")
-            
+
             # Output file (final concatenated MP4 file)
-            output_file = os.path.join(files_dir, f"{self.timestamp}_final_recording.mp4")
-            
+            output_file = os.path.join(files_dir, f"{self.timestamp}_recorded_video.mp4")
+
             # Run FFmpeg command to concatenate files
             concat_command = [
                 'ffmpeg', '-f', 'concat', '-safe', '0', '-i', concat_list_path,
                 '-c', 'copy', output_file
             ]
-            
+
             # Execute the FFmpeg process
             subprocess.run(concat_command, check=True)
-            
+
             logger.info(f"All parts concatenated successfully into {output_file}")
-        
+
         except Exception as e:
             logger.error(f"Error concatenating recording parts: {str(e)}")
 
 
-    def start_recording(self, subject, part=0 ):
-        # self.part = part
+    def get_recorded_files(self):
+        # Directory where parts are saved
+        files_dir = os.path.join(self.media_folderpath, self.subject, self.timestamp)
+
+        # Find all recorded video files
+        recorded_files = sorted(
+            [f for f in os.listdir(files_dir) 
+            if re.match(r'^\d{2}-\d{2}-\d{4}_\d{2}-\d{2}-\d{2}_recorded_video_\d+\.mp4$', f)],
+            key=lambda x: int(re.search(r'_(\d+)\.mp4', x).group(1))
+        )
+        return recorded_files
+    
+
+    def start_recording(self, subject="Science", part=0):
         if part == 0:
             self.update_timestamp()
         self.subject = subject
@@ -112,6 +129,10 @@ class Recorder:
         # Get the audio device info
         self.audio_device = self.get_audio_device_info()
 
+        if not self.audio_device:
+            logger.error("Cannot start recording, audio device not found.")
+            return
+
         # Log and check types of variables
         logger.info(f"Media folder path: {self.media_folderpath} (Type: {type(self.media_folderpath)})")
         logger.info(f"Subject: {self.subject} (Type: {type(self.subject)})")
@@ -119,7 +140,7 @@ class Recorder:
 
         # Construct the filepath
         self.filepath = os.path.join(self.media_folderpath, self.subject, self.timestamp)
-        
+
         # Ensure the filepath is a valid string
         logger.info(f"Constructed filepath: {self.filepath} (Type: {type(self.filepath)})")
 
@@ -135,20 +156,17 @@ class Recorder:
         logger.info(f"Full filepath with filename: {self.filepath} (Type: {type(self.filepath)})")
 
         # Start recording with ffmpeg
-        if self.audio_device:
-            try:
-                self.process = subprocess.Popen(
-                    ['ffmpeg', '-f', 'alsa', '-channels', '1', '-i', self.audio_device, '-i', self.camera_url, 
-                    '-c:v', 'copy', '-c:a', 'aac', self.filepath],
-                    stdin=subprocess.PIPE,
-                    stdout=self.devnull,
-                    stderr=self.devnull         
-                )
-                logger.info(f"Recording started: {self.filename}, File path: {self.filepath}")
-            except Exception as e:
-                logger.error(f"Error starting subprocess: {str(e)}")
-        else:
-            logger.error("Cannot start recording, audio device not found.")
+        try:
+            self.process = subprocess.Popen(
+                ['ffmpeg', '-f', 'alsa', '-channels', '1', '-i', self.audio_device, '-i', self.camera_url,
+                 '-c:v', 'copy', '-c:a', 'aac', self.filepath],
+                stdin=subprocess.PIPE,
+                stdout=self.devnull,
+                stderr=self.devnull
+            )
+            logger.info(f"Recording started: {self.filename}, File path: {self.filepath}")
+        except Exception as e:
+            logger.error(f"Error starting subprocess: {str(e)}")
 
 
     def start_screen_grab(self):
@@ -176,8 +194,8 @@ class Recorder:
             self.process.stdin.flush()
             self.process.wait()
             logger.info("Recording stopped.")
-            # Once the recording stops, concatenate parts
-            self.concat_recording_parts()
+            # self.concat_recording_parts()
+
         else:
             logger.warning("No recording in progress.")
 
@@ -206,6 +224,47 @@ class Recorder:
             logger.info("FFmpeg streaming process terminated on deletion.")
 
 
-# if __name__ == "__main__":
-#     recorder = Recorder()
-#     # Here you can call methods like recorder.start_recording("example_subject")
+if __name__ == "__main__":
+    recorder = Recorder()
+    # Here you can call methods like recorder.start_recording("example_subject")
+    print("started recording")
+    recorder.start_recording()
+    time.sleep(60)
+
+    print("pause recording")
+    recorder.pause_recording()
+    time.sleep(30)
+
+    print("unpause recording")
+    recorder.unpause_recording()
+    time.sleep(30)
+
+    print("pause recording")
+    recorder.pause_recording()
+    time.sleep(30)
+
+    print("unpause recording")
+    recorder.unpause_recording()
+    time.sleep(30)
+
+    print("stopped recording")
+    recorder.stop_recording()
+    time.sleep(10)
+
+        # Once the recording stops, concatenate parts
+    recorded_files = recorder.get_recorded_files()
+
+    if len(recorded_files) == 0:
+        print("No recordings found.")
+    elif len(recorded_files) == 1:
+        # Only one recording found, process it directly
+        single_file = recorded_files[0]
+        print(f"Single file: {single_file}, No need concatination")
+    else:
+        # Multiple recordings found, concatenate them
+        print("Multiple recordings found, concatenating...")
+        # Call your concatenation function here
+        recorder.concat_recording_parts()
+
+   
+         
--- pod/eonpod/pod/static/js/stoprecording_alert.js
+++ pod/eonpod/pod/static/js/stoprecording_alert.js
@@ -1,77 +1,3 @@
-// function showConfirmDialog() {
-//     const modal = document.getElementById("customConfirm");
-//     // const overlay = document.getElementById("modalOverlay");
-//     const startRecordButton = document.getElementById("startRecord");
-//     const stopRecordButton = document.getElementById("stopRecord");
-//     const textLabelRecord = document.getElementById("text-label-record");
-//     const progressBar = document.getElementById("progress-bar1");
-//     const customAlert = document.getElementById("customAlert");
-//     const closeAlertBtn = document.getElementById("closeAlertBtn");
-
-//     // Show the confirm modal and overlay
-//     modal.style.display = "block";
-
-//     // Handle confirm (OK)
-//     document.getElementById("confirmBtn").onclick = async function () {
-//         modal.style.display = "none"; // Close the confirm modal
-//         customAlert.style.display = "block"; // Show the custom alert
-
-//         // Proceed with stopping the recording when the alert is closed
-//         closeAlertBtn.onclick = async function () {
-//             customAlert.style.display = "none"; // Close the custom alert
-
-//             // Stop the recording
-//             try {
-//                 let response = await fetch('/stop_recording_view/', {
-//                     method: 'POST',
-//                     headers: {
-//                         'Content-Type': 'application/json',
-//                         'X-CSRFToken': getCookie('csrftoken')
-//                     }
-//                 });
-
-//                 if (!response.ok) {
-//                     throw new Error('Network response was not ok');
-//                 }
-
-//                 let data = await response.json();
-
-//                 if (data.success) {
-//                     console.log(data.message);
-//                     sendRecordingStatus(false);
-//                     startRecordButton.style.display = "block";
-//                     stopRecordButton.style.display = "none";
-//                     textLabelRecord.textContent = "Start Recording";
-//                     progressBar.style.visibility = "hidden"; // Hide progress bar
-//                     progressBar.style.width = "0%"; // Reset progress bar
-//                     isRecording = false;
-//                 } else {
-//                     console.error("Error message in stop recording: " + data.error);
-//                 }
-//             } catch (error) {
-//                 console.error('Failed to stop recording:', error);
-//             }
-//         };
-//     };
-
-//     // Handle cancel
-//     document.getElementById("cancelBtn").onclick = function () {
-//         modal.style.display = "none"; // Close the modal
-//         console.log('Recording continues.');
-//     };
-
-// }
-
-// // Close custom alert when clicking outside of the alert box
-// window.onclick = function (event) {
-//     const customAlert = document.getElementById("customAlert");
-    
-//     // Close the custom alert if the user clicks outside of it
-//     if (event.target === customAlert) {
-//         customAlert.style.display = "none"; // Close the custom alert
-//     }
-// };
-
 
 function showConfirmDialog() {
     const modal = document.getElementById("customConfirm");
@@ -93,7 +19,7 @@ function showConfirmDialog() {
         setTimeout(async function () {
             customAlert.style.display = "none"; // Hide the custom alert after 10 seconds
             await stopRecording(); // Stop the recording automatically
-        }, 6000); // 10,000 milliseconds = 10 seconds
+        }, 5000); // 10,000 milliseconds = 10 seconds
     };
 
     // Handle cancel
@@ -106,6 +32,14 @@ function showConfirmDialog() {
 // Function to stop the recording
 async function stopRecording() {
     try {
+        document.getElementById("startRecord").style.display = "block";
+        document.getElementById("stopRecord").style.display = "none";
+        document.getElementById("text-label-record").textContent = "Start Recording";
+        document.getElementById("progress-bar1").style.visibility = "hidden"; // Hide progress bar
+        document.getElementById("progress-bar1").style.width = "0%"; // Reset progress bar
+        isRecording = false;
+
+
         let response = await fetch('/stop_recording_view/', {
             method: 'POST',
             headers: {
@@ -115,24 +49,12 @@ async function stopRecording() {
             body: JSON.stringify({ subject: selectedSubject })
         });
 
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-
         let data = await response.json();
 
         console.log(data.message);
-
-        sendRecordingStatus(false);
-        document.getElementById("startRecord").style.display = "block";
-        document.getElementById("stopRecord").style.display = "none";
-        document.getElementById("text-label-record").textContent = "Start Recording";
-        document.getElementById("progress-bar1").style.visibility = "hidden"; // Hide progress bar
-        document.getElementById("progress-bar1").style.width = "0%"; // Reset progress bar
-        isRecording = false;
         
         checkDeviceConnections();
-        
+
     } catch (error) {
         console.error('Failed to stop recording:', error);
     }
