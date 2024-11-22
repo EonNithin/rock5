48e8769676e3ace66862210a0d5bc6782d5d14e9
Nithin <nithin@learneon.com>
2024-11-22 14:09:24 +0530

    save

--- pod/eonpod_setup_scripts/startup_eonpod.sh
+++ pod/eonpod_setup_scripts/startup_eonpod.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-# Serve a simple loading page until the server is ready
-echo "Displaying loading page..."
-chromium-browser --kiosk "file://$HOME/eonpod-ai/pod/eonpod_setup_scripts/loading.html" --disable-pinch --disable-zoom --overscroll-history-navigation=0 &
-CHROMIUM_PID=$!
-
-# Wait for the Django server to become accessible
-until curl -s http://localhost:8000 > /dev/null; do
-    echo "Waiting for server to start..."
-    sleep 1
-done
-
-# Kill the loading page Chromium instance
-echo "Killing loading page..."
-kill "$CHROMIUM_PID" 2>/dev/null
-
 # Navigate to the base directory (eonpod-ai)
 cd "$HOME/eonpod-ai"
 
@@ -37,6 +22,22 @@ if ! lsof -i :8000 | grep LISTEN; then
 else
     echo "Django server already running."
 fi
+
+# Serve a simple loading page until the server is ready
+echo "Displaying loading page..."
+chromium-browser --kiosk "file://$HOME/eonpod-ai/pod/eonpod_setup_scripts/loading.html" --disable-pinch --disable-zoom --overscroll-history-navigation=0 &
+CHROMIUM_PID=$!
+
+# Wait for the Django server to become accessible
+until curl -s http://localhost:8000 > /dev/null; do
+    echo "Waiting for server to start..."
+    sleep 1
+done
+
+# Kill the loading page Chromium instance
+echo "Killing loading page..."
+kill "$CHROMIUM_PID" 2>/dev/null
+
 
 # Open the Django app in Chromium in kiosk mode
 chromium-browser --kiosk "http://localhost:8000" --disable-pinch --disable-zoom --overscroll-history-navigation=0 &
