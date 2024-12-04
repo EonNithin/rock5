cb0e75e7e2ee104b7ff4aff0c6a0b3819eaadcb0
Nithin <nithin@learneon.com>
2024-12-04 20:21:51 +0530

    save change removed exit in script

--- pod/eonpod_setup_scripts/startup_eonpod.sh
+++ pod/eonpod_setup_scripts/startup_eonpod.sh
@@ -25,7 +25,6 @@ else
 fi
 
 
-
 # Run PostgreSQL commands to drop specific tables
 echo "Running PostgreSQL commands..."
 psql postgresql://learneon_dev:12345@localhost:5432/local_eonpod_db <<EOF
@@ -38,8 +37,7 @@ EOF
 if [ $? -eq 0 ]; then
     echo "PostgreSQL commands executed successfully."
 else
-    echo "Failed to execute PostgreSQL commands. Check database connectivity and syntax."
-    exit 1
+    echo "Failed to execute PostgreSQL commands. Check database connectivity and syntax. Continuing..."
 fi
 
 # Execute the shell script to set up the database
@@ -47,16 +45,9 @@ echo "Executing database setup script..."
 DB_SETUP_SCRIPT="$HOME/eonpod-ai/pod/eonpod_setup_scripts/local DB Setup scripts/create_tables_in_DB.sh"
 if [ -f "$DB_SETUP_SCRIPT" ]; then
     chmod +x "$DB_SETUP_SCRIPT"
-    "$DB_SETUP_SCRIPT"
-    if [ $? -eq 0 ]; then
-        echo "Database setup script executed successfully."
-    else
-        echo "Database setup script execution failed."
-        exit 1
-    fi
+    "$DB_SETUP_SCRIPT" || echo "Database setup script execution failed. Continuing..."
 else
-    echo "Database setup script not found: $DB_SETUP_SCRIPT"
-    exit 1
+    echo "Database setup script not found: $DB_SETUP_SCRIPT. Continuing..."
 fi
 
 
